683b066276963c026304bd586ca649f50831f1dc
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-05-24 09:35:07 +0000

    config: seperate url path for crypto app

---
+++ crypto/urls.py
@@ -0,0 +1,16 @@
+# imports django path, django admin and view funtions
+from django.contrib import admin
+from django.urls import path
+from crypto.views import index, registration, Userlogin, view_portfolio
+from crypto.views import log_out
+
+# sets out the path for the relevant templates
+urlpatterns = [
+   path('', index, name='index'),
+   path('admin/', admin.site.urls),
+   path('Templates/registration.html', registration, name='registration'),
+   path('Templates/login.html', Userlogin, name='userlogin'),
+   path('Templates/index.html', index, name='home'),
+   path('Templates/logout.html', log_out, name='logout'),
+   path('Templates/portfolio.html', view_portfolio, name='portfolio'),
+]
--- crypto_portfolio/urls.py
+++ crypto_portfolio/urls.py
@@ -1,15 +1,9 @@
-# imports django path, django admin and funtion names
-from django.contrib import admin
-from django.urls import path
-from crypto.views import index, registration, Userlogin, view_portfolio, log_out
+# imports django path
+from django.urls import path, include
 
-# sets out the path for the relevant templates
+
+# sets out the path for admin and links crypto app urls
 urlpatterns = [
-   path('', index, name='index'),
-   path('Templates/registration.html', registration, name='registration'),
-   path('Templates/login.html', Userlogin, name='userlogin'),
-   path('Templates/index.html', index, name='home'),
-   path('Templates/logout.html', log_out, name='logout'),
-   path('Templates/portfolio.html', view_portfolio, name='portfolio'),
-]
+   path('', include('crypto.urls')),
 
+]
