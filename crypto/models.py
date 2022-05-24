b182b1f904c14f339a9bb3793070df5a8a7ea43e
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-05-24 09:34:01 +0000

    fix: errors

--- crypto/models.py
+++ crypto/models.py
@@ -1,13 +1,14 @@
 # inports django models and user
 from django.db import models
 from django.contrib.auth.models import User
- 
+
+
 # Creates model
 class PortfolioTracker(models.Model):
-   user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-   crypto = models.CharField(max_length=100)
-   symbol = models.CharField(max_length=20, default=None)
-   tokens = models.CharField(max_length=50 , default=None, null=True, blank=True)
- 
-   def __str__(self):
-       return str(self.crypto)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    crypto = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=20, default=None)
+    tokens = models.CharField(max_length=50, default=None, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.crypto)
--- crypto/views.py
+++ crypto/views.py
@@ -4,65 +4,65 @@ import json
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from crypto.forms import signup
-from crypto.models import PortfolioTracker
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+from crypto.forms import Signup
+from crypto.models import PortfolioTracker
 
 
 # view function for home page
 def index(request):
-   api = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C7d'
-   data = requests.get(api).json()
- 
- # returns relevant data to home page table from the api endpoint above
-   return render(request, template_name="index.html", context={'coins':data})
+    api = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C7d'
+    data = requests.get(api).json()
+
+    # returns relevant data to home page table from the api endpoint above
+    return render(request, template_name="index.html", context={'coins':data})
 
 # view function for registration
 def registration(request):
     # the sign up form sends a post request
     if request.method == "POST":
-            form = signup(request.POST)
-             # if the form is valid, it's saved an user is notified
-            if form.is_valid():
-                user = form.save()
-                login(request, user)
-                messages.success(request, "Registration successful.")
-                return redirect("registration")
-            else:
-                 # if the form is invalid user is notfied and registration attempt is not saved
-                messages.error(request, "Registration unsuccessful. Please fill out the form correctly")
-    # the signup form from forms.py 
-    form = signup()
+        form = Signup(request.POST)
+        # if the form is valid, it's saved an user is notified
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("registration")
+        else:
+            # if the form is invalid user is notfied and registration attempt is not saved
+            messages.error(request, "Registration unsuccessful. Please fill out the form correctly")
+    # the signup form from forms.py
+    form = Signup()
      #  http respone is then retured to the browser
     return render (request=request, template_name="registration.html", context={"form":form})
 #  view function for login, django authentication form is used
 def Userlogin(request):
     if request.method == "POST":
-            form = AuthenticationForm(request, data=request.POST)
-            #  valid form clears form inputs
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(username=username, password=password)
-                #  user is notified once successfully logged in
-                if user is not None:
-                    login(request, user)
-                    messages.info(request, f"You are now logged in as {username}.")
-                    return redirect("userlogin")
-                else:
-                    #  if login is uncessful user is notified
-                    messages.error(request,"Incorrect username or password, please try again.")
+        form = AuthenticationForm(request, data=request.POST)
+        #  valid form clears form inputs
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            #  user is notified once successfully logged in
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("userlogin")
             else:
-                messages.error(request,"Invalid username or password,please try again.")
+                #  if login is uncessful user is notified
+                messages.error(request,"Incorrect username or password, please try again.")
+        else:
+            messages.error(request,"Invalid username or password,please try again.")
     form = AuthenticationForm()
     return render(request=request, template_name="login.html", context={"login":form})
 #  view funtion for logging out
 def log_out(request):
     #  django logout is used and user notified once they log out sucessfully
     logout(request)
-    messages.info(request, "You have successfully logged out.")  
+    messages.info(request, "You have successfully logged out.")
     #  user is then redirected to the login page
     return redirect("userlogin")
     return render(request=request, template_name='logout.html')
@@ -76,7 +76,9 @@ def view_portfolio(request):
         token = request.POST.get('number_tokens')
         # when a coin name is recieved a portfolio object is created based on the users input
         if coin:
-            PortfolioTracker.objects.create( user=request.user, crypto=coin, symbol=symbol, tokens=token)
+            PortfolioTracker.objects.create(
+                                            user=request.user, crypto=coin,
+                                            symbol=symbol, tokens=token)
              # user returned to the same page (portfolio) so that they can view their portfolio data
             return HttpResponseRedirect(reverse("portfolio"))
 
@@ -91,13 +93,13 @@ def view_portfolio(request):
         year_list =[]
         highs_list =[]
         lows_list =[]
-        
+
         #  user coin input is added to api data to populate lists
         for objs in portfolio_objs:
             url = f'https://api.coingecko.com/api/v3/coins/{objs.crypto}'
             data = requests.get(url).json()
 
- 
+
             float_currency_amount = float(objs.tokens)
             price = data['market_data']['current_price']['usd']
             day= data['market_data']['price_change_percentage_24h_in_currency']['usd']
@@ -107,7 +109,7 @@ def view_portfolio(request):
             highs=data['market_data']['high_24h']['usd']
             lows=data['market_data']['low_24h']['usd']
             # users token(s) value is calculated in usd
-            total_value = float_currency_amount * price 
+            total_value = float_currency_amount * price
             # appends the collected data to the relvant list
             name_list.append(objs.crypto)
             value_list.append(total_value)
@@ -124,8 +126,3 @@ def view_portfolio(request):
             'datalist':complete_list
         }
         return render(request=request, template_name="portfolio.html" , context=coin_data)
-
-
-
-    
-
