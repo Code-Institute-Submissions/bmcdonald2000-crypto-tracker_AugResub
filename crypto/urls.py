4d8433de1a85f1f5b342d4694f05e993e22d62c2
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-05-29 06:00:06 +0000

    Add: Top 100 link to nav bar

--- crypto/urls.py
+++ crypto/urls.py
@@ -2,15 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 from crypto.views import index, registration, Userlogin, view_portfolio
-from crypto.views import log_out
+from crypto.views import log_out, top100
 
 # sets out the path for the relevant templates
 urlpatterns = [
-   path('', index, name='index'),
    path('admin/', admin.site.urls),
    path('Templates/registration.html', registration, name='registration'),
    path('Templates/login.html', Userlogin, name='userlogin'),
    path('Templates/index.html', index, name='home'),
    path('Templates/logout.html', log_out, name='logout'),
    path('Templates/portfolio.html', view_portfolio, name='portfolio'),
+   path('Templates/top100.html', top100, name='top100'),
 ]
