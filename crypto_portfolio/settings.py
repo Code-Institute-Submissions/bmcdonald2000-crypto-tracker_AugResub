c63d292f540eb1bb15f328f0bc51dc33c140cd45
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-08-07 15:09:08 +0000

    disable static files

--- crypto_portfolio/settings.py
+++ crypto_portfolio/settings.py
@@ -133,4 +133,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_heroku.settings(locals(),staticfiles=False)
