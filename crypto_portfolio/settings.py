45d1919f92e56c974b9a408002401357d6e0c909
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-08-07 13:34:18 +0000

    config: turn off debug mode

--- crypto_portfolio/settings.py
+++ crypto_portfolio/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['']
 
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'crypto_portfolio.urls'
